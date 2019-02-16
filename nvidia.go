823fcc97d921533578ff2a901c2772bd015ff68e
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2019-02-15 16:49:26 -0800

    fork real device into fake devices to enable gpu sharing

--- nvidia.go
+++ nvidia.go
@@ -3,7 +3,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/NVIDIA/gpu-monitoring-tools/bindings/go/nvml"
@@ -18,18 +21,50 @@ func check(err error) {
 	}
 }
 
+func generateFakeDeviceID(realID string, fakeCounter uint) string {
+	return fmt.Sprintf("%s-_-%d", realID, fakeCounter)
+
+}
+
+func extractRealDeviceID (fakeDeviceID string) string {
+	return strings.Split(fakeDeviceID, "-_-")[0]
+}
+
+func getNumberContainersPerGPU() (numGPU uint) {
+	numGPU = 1 // default value
+	strNum, present := os.LookupEnv(envNumberContainersPerGPU)
+	if !present {
+		return
+	}
+	rawNumGPU, err := strconv.Atoi(strNum)
+	if err != nil {
+		log.Panicf("Fatal: Could not parse %s environment variable: %v\n", envNumberContainersPerGPU, err)
+	}
+	if rawNumGPU < 1 {
+		log.Panicf("Fatal: invalid %s environment variable value: %v\n", envNumberContainersPerGPU, rawNumGPU")
+	}
+
+	return
+}
+
 func getDevices() []*pluginapi.Device {
 	n, err := nvml.GetDeviceCount()
 	check(err)
 
 	var devs []*pluginapi.Device
-	for i := uint(0); i < n; i++ {
-		d, err := nvml.NewDeviceLite(i)
-		check(err)
-		devs = append(devs, &pluginapi.Device{
-			ID:     d.UUID,
-			Health: pluginapi.Healthy,
-		})
+	log.Println("List devices")
+	for j := uint(0); j < getNumberContainersPerGPU(); j++ {
+		for i := uint(0); i < n; i++ {
+			d, err := nvml.NewDeviceLite(i)
+			check(err)
+			fakeID := generateFakeDeviceID(d.UUID, j)
+			log.Println("Device ID:", fakeID)
+			devs = append(devs, &pluginapi.Device{
+				ID:     fakeID,
+				Health: pluginapi.Healthy,
+			})
+		}
+
 	}
 
 	return devs
@@ -49,9 +84,11 @@ func watchXIDs(ctx context.Context, devs []*pluginapi.Device, xids chan<- *plugi
 	defer nvml.DeleteEventSet(eventSet)
 
 	for _, d := range devs {
-		err := nvml.RegisterEventForDevice(eventSet, nvml.XidCriticalError, d.ID)
+		realDeviceID := extractRealDeviceID(d.ID)
+
+		err := nvml.RegisterEventForDevice(eventSet, nvml.XidCriticalError, realDeviceID)
 		if err != nil && strings.HasSuffix(err.Error(), "Not Supported") {
-			log.Printf("Warning: %s is too old to support healthchecking: %s. Marking it unhealthy.", d.ID, err)
+			log.Printf("Warning: %s (%s) is too old to support healthchecking: %s. Marking it unhealthy.", realDeviceID, d.ID, err)
 
 			xids <- d
 			continue
@@ -90,7 +127,7 @@ func watchXIDs(ctx context.Context, devs []*pluginapi.Device, xids chan<- *plugi
 		}
 
 		for _, d := range devs {
-			if d.ID == *e.UUID {
+			if extractRealDeviceID(d.ID) == *e.UUID {
 				xids <- d
 			}
 		}

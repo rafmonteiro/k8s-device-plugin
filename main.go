5990d92e7a3e45233824b4bf2fc7a2b4bdb3faf4
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2019-02-15 16:46:43 -0800

    update help messages with new path

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+
 	"log"
 	"os"
 	"syscall"
@@ -17,8 +18,8 @@ func main() {
 	if err := nvml.Init(); err != nil {
 		log.Printf("Failed to initialize NVML: %s.", err)
 		log.Printf("If this is a GPU node, did you set the docker default runtime to `nvidia`?")
-		log.Printf("You can check the prerequisites at: https://github.com/NVIDIA/k8s-device-plugin#prerequisites")
-		log.Printf("You can learn how to set the runtime at: https://github.com/NVIDIA/k8s-device-plugin#quick-start")
+		log.Printf("You can check the prerequisites at: https://github.com/rafmonteiro/k8s-device-plugin#prerequisites")
+		log.Printf("You can learn how to set the runtime at: https://github.com/rafmonteiro/k8s-device-plugin#quick-start")
 
 		select {}
 	}
@@ -54,8 +55,8 @@ L:
 			devicePlugin = NewNvidiaDevicePlugin()
 			if err := devicePlugin.Serve(); err != nil {
 				log.Println("Could not contact Kubelet, retrying. Did you enable the device plugin feature gate?")
-				log.Printf("You can check the prerequisites at: https://github.com/NVIDIA/k8s-device-plugin#prerequisites")
-				log.Printf("You can learn how to set the runtime at: https://github.com/NVIDIA/k8s-device-plugin#quick-start")
+				log.Printf("You can check the prerequisites at: https://github.com/rafmonteiro/k8s-device-plugin#prerequisites")
+				log.Printf("You can learn how to set the runtime at: https://github.com/rafmonteiro/k8s-device-plugin#quick-start")
 			} else {
 				restart = false
 			}
